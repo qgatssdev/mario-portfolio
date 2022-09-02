11c0fdd10bbf981e7cdad05d95c4e8646e644ca2
Uche Omodu <chivoomodu@gmail.com>
2022-09-02 07:22:34 +0100

    Initializing works

---
+++ src/components/Works/Works.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Works = () => {
+  return (
+    <section>
+      <div>
+        <h1>Works.</h1>
+        <p>
+          <span>
+            <img src={require('../../assets/Rectangle 1.svg').default} alt="" />
+          </span>
+          Here's a sample of projects I have worked on.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default Works;
