d2a422b2e2f858163aafb25f8ec16df26023a086
Uche Omodu <chivoomodu@gmail.com>
2022-09-06 23:12:06 +0100

    building mobile header for responsiveness

--- src/components/MobileHeader/MobileHeader.tsx
+++ src/components/MobileHeader/MobileHeader.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import './MobileHeader.scss';
 const MobileHeader = () => {
   return (
-    <div>MobileHeader</div>
-  )
-}
+    <div className="mobileHeader__root">
+      <div>
+        <img src={require('../../assets/ioIcon 2.svg').default} alt="" />
+      </div>
+      <div>
+        <img src={require('../../assets/Group 1920.svg').default} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default MobileHeader
+export default MobileHeader;
