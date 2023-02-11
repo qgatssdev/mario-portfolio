1a7aff3fa0b9e2a889dfc8219e0588825e1332d2
qgatssdev <chivoomodu@gmail.com>
2023-02-11 22:54:44 +0100

    Remove framer from component

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,5 +1,4 @@
 import './Works.scss';
-import { motion } from 'framer-motion';
 
 const Works = () => {
   const openInNewTab = (url: any) => {
@@ -7,41 +6,32 @@ const Works = () => {
   };
 
   return (
-    <motion.div
-      whileInView={{ scale: 1, x: 0 }}
-      initial={{ scale: 0.7, x: -1200 }}
-      transition={{
-        delay: 0.5,
-        bounce: 0.5,
-        ease: 'easeInOut',
-        duration: 0.6,
-      }}
-    >
-      <section className="works__root">
-        <div className="work__root__section1">
+    <div>
+      <section className='works__root'>
+        <div className='work__root__section1'>
           <h1>Works.</h1>
           <p>
             <span>
               <img
                 src={require('../../assets/Rectangle 1.svg').default}
-                alt=""
+                alt=''
               />
             </span>
             Here's a sample of projects I have worked on.
           </p>
         </div>
-        <div className="work__root__section2">
-          <div className="section2__items">
-            <div className="section__flex">
+        <div className='work__root__section2'>
+          <div className='section2__items'>
+            <div className='section__flex'>
               <p>NeüRMS Web App Design</p>
-              <img src={require('../../assets/01.svg').default} alt="" />
+              <img src={require('../../assets/01.svg').default} alt='' />
             </div>
           </div>
           <div
             className={'section2__items'}
             onClick={() => openInNewTab('http://app.neurms.com')}
           >
-            <img src={require('../../assets/neums.png')} alt="" />
+            <img src={require('../../assets/neums.png')} alt='' />
           </div>
           <div
             className={'section2__items'}
@@ -49,40 +39,40 @@ const Works = () => {
               openInNewTab('https://softstructures-velsta.netlify.app/')
             }
           >
-            <img src={require('../../assets/siatech.png')} alt="" />
+            <img src={require('../../assets/siatech.png')} alt='' />
           </div>
-          <div className="section2__items">
-            <div className="section__flex">
+          <div className='section2__items'>
+            <div className='section__flex'>
               <p>Velsta Services Website Design</p>
-              <img src={require('../../assets/02.svg').default} alt="" />
+              <img src={require('../../assets/02.svg').default} alt='' />
             </div>
           </div>
-          <div className="section2__items">
-            <div className="section__flex">
+          <div className='section2__items'>
+            <div className='section__flex'>
               <p>Siatech Africa Website Design</p>
-              <img src={require('../../assets/03.svg').default} alt="" />
+              <img src={require('../../assets/03.svg').default} alt='' />
             </div>
           </div>
           <div
             className={'section2__items'}
             onClick={() => openInNewTab('https://siatech.netlify.app/')}
           >
-            <img src={require('../../assets/music player.png')} alt="" />
+            <img src={require('../../assets/music player.png')} alt='' />
           </div>
           <div
-            className="section2__items"
+            className='section2__items'
             onClick={() =>
               openInNewTab(
                 'https://www.figma.com/proto/NPrqmW4pdTnbg1t08AQn0H/MusicPlayer?page-id=0%3A1&node-id=2%3A2&viewport=401%2C337%2C0.08&scaling=min-zoom&starting-point-node-id=13%3A1487'
               )
             }
           >
-            <img src={require('../../assets/music.png')} alt="" />
+            <img src={require('../../assets/music.png')} alt='' />
           </div>
-          <div className="section2__items">
-            <div className="section__flex">
+          <div className='section2__items'>
+            <div className='section__flex'>
               <p>Music Player Desktop Application</p>
-              <img src={require('../../assets/04.svg').default} alt="" />
+              <img src={require('../../assets/04.svg').default} alt='' />
             </div>
           </div>
           {/* <div className="section2__items">
@@ -99,7 +89,7 @@ const Works = () => {
           </div> */}
         </div>
       </section>
-    </motion.div>
+    </div>
   );
 };
 
