3a52b5137cca03f3c2f0a918782a5ef21cfd0b44
qgatssdev <chivoomodu@gmail.com>
2023-02-15 23:54:06 +0100

    Remove framer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,37 +1,27 @@
 import './Footer.scss';
-import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
-    <motion.div
-      whileInView={{ x: 0, opacity: 1 }}
-      initial={{ x: -10, opacity: 0 }}
-      transition={{
-        delay: 1,
-        bounce: 0.5,
-        ease: 'easeInOut',
-        duration: 0.6,
-      }}
-    >
-      <section className="footer__root">
-        <section className="footer__links">
-          <div className="links__1">
-            <img src={require('../../assets/ioIcon 2.svg').default} alt="" />
+    <div>
+      <section className='footer__root'>
+        <section className='footer__links'>
+          <div className='links__1'>
+            <img src={require('../../assets/ioIcon 2.svg').default} alt='' />
             <p>Olurotimi.100@gmail.com</p>
-            <div className="links__1__logos">
+            <div className='links__1__logos'>
               <img
                 src={require('../../assets/behance (1) 1.svg').default}
-                alt=""
+                alt=''
               />
-              <img src={require('../../assets/in 1.svg').default} alt="" />
+              <img src={require('../../assets/in 1.svg').default} alt='' />
               <img
                 src={require('../../assets/instagram 1.svg').default}
-                alt=""
+                alt=''
               />
-              <img src={require('../../assets/twitter 1.svg').default} alt="" />
+              <img src={require('../../assets/twitter 1.svg').default} alt='' />
             </div>
           </div>
-          <div className="links__2">
+          <div className='links__2'>
             <p>
               Feel free to reach out.
               <br /> I'm looking forward to <br />
@@ -39,16 +29,16 @@ const Footer = () => {
             </p>
           </div>
         </section>
-        <div className="recognition">
-          <p className="recognition__developer">
+        <div className='recognition'>
+          <p className='recognition__developer'>
             Developed by <span>Uche Omodu (qgatss)</span>
           </p>
-          <p className="recogntion__rights">
+          <p className='recogntion__rights'>
             2022 © Israel Olurotimi. All rights reserved
           </p>
         </div>
       </section>
-    </motion.div>
+    </div>
   );
 };
 
